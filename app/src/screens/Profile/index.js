e6ca7ee5e556e93957d298bffe22686ef96def1c
Guilherme Pimenta <guialvespimenta27@gmail.com>
2021-01-20 01:46:07 -0200

    PreLoad e SignIn Piloto

---
+++ app/src/screens/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+const Appointments = () => {
+  return (
+    <Container>
+      <Text>Profile Screen</Text>
+    </Container>
+  );
+};
+
+export default Appointments;
